4bfb999b63dc18963b1fc8b499f2d3b5597cf705
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-04 18:06:05 +0530

    route_constants.dart : constant rotes add

---
+++ lib/routes/route_constants.dart
@@ -0,0 +1,7 @@
+
+// Constant String
+
+const String kFirstPage = '/' ;
+const String kSecondPage = '/second_screen' ;
+const String kThirdPage = '/third_screen' ;
+const String kFourthPage = '/fourth_screen' ;
