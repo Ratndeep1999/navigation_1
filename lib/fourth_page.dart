327c001eb9b44179e712107739bb63b3c3595ead
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-04 18:08:51 +0530

    fourth_page.dart : previous data display, popUntil() button, pushNamedAndRemoveUntil() button, page replace fourth to second, pop() button

--- lib/fourth_page.dart
+++ lib/fourth_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_1/routes/route_constants.dart';
 
 class FourthPage extends StatefulWidget {
   const FourthPage({super.key});
@@ -10,6 +11,102 @@ class FourthPage extends StatefulWidget {
 class _FourthPageState extends State<FourthPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final Map? receiveUpdatedData = ModalRoute.of(context)?.settings.arguments as Map? ;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fourth Screen'),
+        centerTitle: true,
+        // it disable (remove) previous arrow button
+        automaticallyImplyLeading: false,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Name : ${receiveUpdatedData?['name'] ?? 'Not Provided'}', style: TextStyle(fontSize: 30)),
+                Text('Age : ${receiveUpdatedData?['age'] ?? 'Not Provided'}', style: TextStyle(fontSize: 30)),
+                Text('Gender : ${receiveUpdatedData?['gender'] ?? 'Not Provided'}', style: TextStyle(fontSize: 30)),
+
+                SizedBox(height: 40),
+                Text('This pop all pages from stack and drop to first page'),
+                SizedBox(height: 10),
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.popUntil(context, ModalRoute.withName('/'));
+                    },
+                    child: Text(
+                      '<< First Screen',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 40),
+                Text('This pop pages until last and then add Second page '),
+                SizedBox(height: 10),
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.pushNamedAndRemoveUntil(
+                        context,
+                        kSecondPage,  // new route at top of stack
+                        ModalRoute.withName(kFirstPage), // remove routes until condition meet
+                      );
+                    },
+                    child: Text(
+                      '<<First Screen <<Second Screen ',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 40),
+                Text('It replace Fourth page to Second page in stack'),
+                SizedBox(height: 10),
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, kSecondPage);
+                    },
+                    child: Text(
+                      '<< Second Screen',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 40),
+                Text('This pop Fourth page (current) from stack'),
+                SizedBox(height: 10),
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      final dataToPass = {
+                        'name': receiveUpdatedData?['name'] ?? 'Not Provided',
+                        'age': receiveUpdatedData?['age']?.toString() ?? 'Not Provided',
+                        'gender': receiveUpdatedData?['gender'] ?? 'Not Provided',
+                      };
+                      Navigator.pop(context,dataToPass); // Pass data back to ThirdPage
+                    },
+                    child: Text(
+                      '<<Previous Screen',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
