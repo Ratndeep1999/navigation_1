6a348f5b4dcb13532ec99bb80e2d747f86a0f281
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-04 18:00:15 +0530

    first_page.dart : text and one button add to navigate second screen

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_1/routes/route_constants.dart';
 
 class FirstPage extends StatefulWidget {
   const FirstPage({super.key});
@@ -10,6 +11,32 @@ class FirstPage extends StatefulWidget {
 class _FirstPageState extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('First Screen'), centerTitle: true),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('This add second page in stack'),
+              SizedBox(height: 10,),
+              SizedBox(
+                height: 50,
+                child: OutlinedButton(
+                  onPressed: () {
+                    // different syntax to define route
+                    Navigator.of(context).pushNamed(kSecondPage);
+                  },
+                  child: Text(
+                    "Second Screen>>",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
